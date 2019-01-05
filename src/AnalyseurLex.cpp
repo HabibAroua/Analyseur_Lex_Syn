deff0ace41398ab44a45cada50e255409bf5ef0e
HabibAroua <habibha.aroua82@gmail.com>
2019-01-05 22:32:47 +0100

    edit code

--- src/AnalyseurLex.cpp
+++ src/AnalyseurLex.cpp
@@ -7,54 +7,51 @@ using namespace std ;
 
 class AnalyseurLex1
 {
-        //Les mots clé
+    //Les mots clé
 
-        public: const string KEY="KEY";
-        public: const string TYPE="TYPE";
-	    public: const string ID="ID";
-	    public: const string PRTO="PRTO";
-	    public: const string PRIF="PRIF";
-	    public: const string CRO="CRO";
-        public: const string CRF="CRF";
-	    public: const string EGA="EGA";
-	    public: const string AFC="AFC";
-	    public: const string PVR="PVR";
-	    public: const string PGE="PGE";
-        public: const string PGQ="PGQ";
-	    public: const string PPE="PPE";
-	    public: const string PPQ="PPQ";
-	    public: const string PLUS="PLUS";
-        public: const string MOIS="MOIS";
-	    public: const string FOIS="FOIS";
-        public: const string DIV="DIV";
-        public: const string DIF="DIF";
-	    public: const string NOT="NOT";
-	    public: const string ET="ET";
-	    public: const string OU="OU";
-	    public: const string DEF="DEF";
-	    public: const string VIR="VIR";
+    public: const string KEY="KEY";
+    public: const string TYPE="TYPE";
+	public: const string ID="ID";
+	public: const string PRTO="PRTO";
+	public: const string PRIF="PRIF";
+	public: const string CRO="CRO";
+    public: const string CRF="CRF";
+	public: const string EGA="EGA";
+	public: const string AFC="AFC";
+	public: const string PVR="PVR";
+	public: const string PGE="PGE";
+    public: const string PGQ="PGQ";
+	public: const string PPE="PPE";
+	public: const string PPQ="PPQ";
+	public: const string PLUS="PLUS";
+    public: const string MOIS="MOIS";
+	public: const string FOIS="FOIS";
+    public: const string DIV="DIV";
+    public: const string DIF="DIF";
+	public: const string NOT="NOT";
+	public: const string ET="ET";
+	public: const string OU="OU";
+	public: const string DEF="DEF";
+	public: const string VIR="VIR";
 
+    //Global variable pour analyser ce grammaire lexical
+	public :  static int ligne ;
+    public :  static int etat;
+    public :  static int pos;
+	public :  char car;
+    public : string tabLexeme [10] ;
+    public : static int offset;
+	public : bool succes ;
 
-
-
-        //Global variable pour analyser ce grammaire lexical
-	    public :  static int ligne ;
-        public :  static int etat;
-     	public :  static int pos;
-	    public :  char car;
-        public : string tabLexeme [10] ;
-        public : static int offset;
-	    public : bool succes ;
-
-        public : void init()
-        {
-            //initialiser les variables analyse lex
-            this->ligne=1;
-            this->etat=AnalyseurLex::ETAT_0;
-            this->pos=0;
-            this->offset=0;
-            this->succes=false;
-        }
+    public : void init()
+    {
+        //initialiser les variables analyse lex
+        this->ligne=1;
+        this->etat=AnalyseurLex::ETAT_0;
+        this->pos=0;
+        this->offset=0;
+        this->succes=false;
+    }
 
 
     public: void initTab()
@@ -79,7 +76,6 @@ class AnalyseurLex1
         tabLexeme[17] = ":";
         tabLexeme[18] = ";";
     }
-
 
 };
 
