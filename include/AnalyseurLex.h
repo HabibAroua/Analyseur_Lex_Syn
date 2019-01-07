d2f8d1b4a540d36b677af9c4e4069f72be71c3c0
HabibAroua <habibha.aroua82@gmail.com>
2019-01-07 02:49:30 +0100

    edit const

--- include/AnalyseurLex.h
+++ include/AnalyseurLex.h
@@ -10,45 +10,42 @@ class AnalyseurLex
         AnalyseurLex();
         //déclarer les ensemples des états
 
-        public: static const int ETAT_0=0;
-        public: static const int ETAT_1=1;
-        public: static const int ETAT_2=1;
-        public: static const int ETAT_3=1;
-        public: static const int ETAT_4=1;
-        public: static const int ETAT_5=1;
-        public: static const int ETAT_6=1;
-        public: static const int ETAT_7=1;
-        public: static const int ETAT_8=1;
-        public: static const int ETAT_9=1;
-        public: static const int ETAT_10=1;
-        public: static const int ETAT_11=0;
-        public: static const int ETAT_12=1;
-        public: static const int ETAT_13=1;
-        public: static const int ETAT_14=1;
-        public: static const int ETAT_15=1;
-        public: static const int ETAT_16=1;
-        public: static const int ETAT_17=1;
-        public: static const int ETAT_18=1;
-        public: static const int ETAT_19=1;
-        public: static const int ETAT_20=1;
-        public: static const int ETAT_21=0;
-        public: static const int ETAT_22=1;
-        public: static const int ETAT_23=1;
-        public: static const int ETAT_24=1;
-        public: static const int ETAT_25=1;
-        public: static const int ETAT_26=1;
-        public: static const int ETAT_27=1;
-        public: static const int ETAT_28=1;
-        public: static const int ETAT_29=1;
-        public: static const int ETAT_30=1;
-        public: static const int ETAT_31=1;
-        public: static const int ETAT_32=0;
-        public: static const int ETAT_33=1;
-        public: static const int ETAT_34=1;
-        public: static const int PUIS=1;
-
-
-
+        public: static const int ETAT_0=0; //etat initial
+        public: static const int ETAT_1=1; //is Alpha
+        public: static const int ETAT_2=2;
+        public: static const int ETAT_3=3; // ;
+        public: static const int ETAT_4=4; //is Num
+        public: static const int ETAT_5=5;
+        public: static const int ETAT_6=6;
+        public: static const int ETAT_7=7;
+        public: static const int ETAT_8=8;
+        public: static const int ETAT_9=9; //(
+        public: static const int ETAT_10=10; //)
+        public: static const int ETAT_11=11;
+        public: static const int ETAT_12=12;
+        public: static const int ETAT_13=13; //=
+        public: static const int ETAT_14=14;
+        public: static const int ETAT_15=15;
+        public: static const int ETAT_16=16; //>
+        public: static const int ETAT_17=17;
+        public: static const int ETAT_18=18;
+        public: static const int ETAT_19=19; //<
+        public: static const int ETAT_20=20;
+        public: static const int ETAT_21=21;
+        public: static const int ETAT_22=22; //+
+        public: static const int ETAT_23=23; //-
+        public: static const int ETAT_24=24; //*
+        public: static const int ETAT_25=25; //  / div
+        public: static const int ETAT_26=26; //  !
+        public: static const int ETAT_27=27;
+        public: static const int ETAT_28=28;
+        public: static const int ETAT_29=29; //&
+        public: static const int ETAT_30=30;
+        public: static const int ETAT_31=31; // |
+        public: static const int ETAT_32=32;
+        public: static const int ETAT_33=33; // :
+        public: static const int ETAT_34=34; // ','
+        public: static const int PUIS=100;
 };
 
 #endif // ANALYSEURLEX_H
--- src/AnalyseurLex.cpp
+++ src/AnalyseurLex.cpp
@@ -129,4 +129,6 @@ class AnalyseurLex1
         return false;
     }
 
+
+
 };
